7a8d1a22c9f86964faf10535cf909e3870a88ad0
abfatahi <abfatahi.iaf@gmail.com>
2022-02-10 22:41:26 +0100

    Fix: sidebar

--- src/layouts/DashboardLayout/Content.jsx
+++ src/layouts/DashboardLayout/Content.jsx
@@ -3,7 +3,13 @@ import styled from 'styled-components';
 import { FaChevronCircleLeft } from 'react-icons/fa';
 
 const Content = (props) => {
-  return <Container>{props.content}</Container>;
+  const tab = localStorage.getItem('tab');
+  return (
+    <Container>
+      <h1>{tab}</h1>
+      {props.content}
+    </Container>
+  );
 };
 
 export default Content;
@@ -13,9 +19,9 @@ const Container = styled.div`
   top: 64px;
   left: 250px;
   width: calc(100vw - 250px);
-  height: -webkit-calc(100% - 64px);
-  height: -moz-calc(100% - 64px);
-  min-height: calc(100vh - 64px);
+  height: -webkit-calc(100%);
+  height: -moz-calc(100%);
+  min-height: calc(100vh);
   background: #efefef;
   padding: 2rem 3rem;
   color: #666666;
--- src/layouts/DashboardLayout/Sidebar.jsx
+++ src/layouts/DashboardLayout/Sidebar.jsx
@@ -5,41 +5,66 @@ import {
   FaWallet,
   FaExchangeAlt,
   FaSignOutAlt,
+  FaUserAlt,
 } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-  const activeTab = sessionStorage.getItem('tab');
+  const [activeTab, setActiveTab] = React.useState('Dashboard');
+
+  React.useEffect(() => {
+    const tab = localStorage.getItem('tab');
+    setActiveTab(tab);
+  }, []);
+
   return (
     <Container>
-      {sidebarTabs.map((tab) => {
-        const { id, name, icon, link } = tab;
-        return (
-          <SidebarTabs
-            key={id}
-            onClick={() => {
-              sessionStorage.setItem('tab', name);
-            }}
-            className={activeTab === name ? 'active' : ''}
-            to={link}
-          >
-            <div className='icon'>{icon}</div>
-            <p>{name}</p>
-          </SidebarTabs>
-        );
-      })}
-      <br />
-      <br />
-      <br />
+      <div className='user_info'>
+        <FaUserAlt className='avatar' />
+        <div className='group'>
+          <h2>Ishaq Abdulfatahi</h2>
+          <h3>0154305609</h3>
+        </div>
+      </div>
+
       <SidebarTabs
         onClick={() => {
-          sessionStorage.clear();
+          localStorage.setItem('tab', 'Dashboard');
         }}
+        className={activeTab === 'Dashboard' ? 'active' : ''}
+        to={'/'}
+      >
+        <FaBuffer className='icon' />
+        <p>Dashboard</p>
+      </SidebarTabs>
+      <SidebarTabs
+        onClick={() => {
+          localStorage.setItem('tab', 'Accounts');
+        }}
+        className={activeTab === 'Accounts' ? 'active' : ''}
+        to={'/'}
+      >
+        <FaWallet className='icon' />
+        <p>Accounts</p>
+      </SidebarTabs>
+      <SidebarTabs
+        onClick={() => {
+          localStorage.setItem('tab', 'Transfers');
+        }}
+        className={activeTab === 'Transfers' ? 'active' : ''}
+        to={'/'}
+      >
+        <FaExchangeAlt className='icon' />
+        <p>Transfers</p>
+      </SidebarTabs>
+      <SidebarTabs
+        onClick={() => {
+          localStorage.clear();
+        }}
+        className='logout__btn'
         to={'/login'}
       >
-        <div className='icon'>
-          <FaSignOutAlt />
-        </div>
+        <FaSignOutAlt className='icon' />
         <p>Logout</p>
       </SidebarTabs>
     </Container>
@@ -50,20 +75,27 @@ export default Sidebar;
 
 const Container = styled.div`
   position: fixed;
-  top: 80px;
+  top: 64px;
   left: 0;
   height: calc(100vh - 64px);
   width: 250px;
   background: #3e464e;
-  padding: 4rem 1rem 0;
+  padding: 1rem 0 0;
   display: flex;
   flex-direction: column;
   row-gap: 1rem;
 
-  .active {
-    background: #e20b8c !important;
+  .logout__btn {
+    position: absolute;
+    bottom: 5rem;
+  }
 
-    p {
+  .active {
+    background: #e24307 !important;
+    border-left: 5px solid #fff !important;
+
+    p,
+    .icon {
       color: #fff !important;
     }
 
@@ -71,56 +103,65 @@ const Container = styled.div`
       opacity: 0.7;
     }
   }
+
+  .user_info {
+    display: flex;
+    width: 100%;
+    padding: 1rem;
+    align-items: center;
+    margin-bottom: 3rem;
+    gap: 0.5rem;
+
+    .avatar {
+      width: 36px;
+      height: 36px;
+      color: #bdbdbd;
+      border-radius: 50%;
+      border: 1px solid #bdbdbd;
+      padding: 5px;
+    }
+    .group {
+      display: flex;
+      flex-direction: column;
+
+      h2 {
+        font-size: 1rem;
+        color: #e24307;
+      }
+
+      h3 {
+        font-size: 1rem;
+        color: #fff;
+      }
+    }
+  }
 `;
 
 const SidebarTabs = styled(Link)`
   display: flex;
   align-items: center;
-  border-radius: 10px;
-  padding: 1em 1em 1em 0.8em;
+  padding: 1em 1em 1em 2em;
   cursor: pointer;
   text-decoration: none;
 
   .icon {
-    width: 2em;
-    height: 2em;
+    width: 1.5em;
+    height: 1.5em;
     margin-right: 1rem;
+    color: #bdbdbd;
   }
 
   p {
     font-style: normal;
-    font-weight: bold;
     font-size: 1rem;
     line-height: 23px;
     letter-spacing: 0.0015em;
-    color: #666666;
+    color: #bdbdbd;
     margin: 0;
     padding: 0;
   }
 
   :hover {
     opacity: 0.7;
-    background: #efefef;
   }
 `;
-
-const sidebarTabs = [
-  {
-    id: 1,
-    name: 'Overview',
-    icon: <FaBuffer />,
-    link: '/dashboard',
-  },
-  {
-    id: 2,
-    name: 'Account',
-    icon: <FaWallet />,
-    link: '/account',
-  },
-  {
-    id: 3,
-    name: 'Transfers',
-    icon: <FaExchangeAlt />,
-    link: '/transfers',
-  },
-];
